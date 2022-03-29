fc87a5bdcdb7589bf23e48ab3ae7f466fe02411e
석성희 <sunghee119z@naver.com>
2022-03-29 22:13:50 +0900

    네이버
    네이버

--- src/main/java/sunghee/web/controller/IndexController.java
+++ src/main/java/sunghee/web/controller/IndexController.java
@@ -5,17 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import sunghee.config.auth.LoginUser;
+import sunghee.config.auth.dto.SessionUser;
 import sunghee.service.posts.PostsService;
 import sunghee.web.dto.PostsResponseDto;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.postsList());
+
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
